cf4451d5d9d01947efe4a62a4b173dff9f0f0254
Dima-js <dimd98@ukr.net>
2017-05-29 23:25:09 +0300

    add block

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,36 +62,45 @@ module.exports = function (grunt) {
         }
       }
     },
-    htmlbuild: {
-        dist: {
-            src: 'index.html',
-            dest: './',
-            options: {
-                beautify: true,
-                scripts: {
-                    bundle: [
-                        'js/*.js'
-                    ],
-                    main: 'scripts/common.js'
-                },
-                styles: {
-                    bundle: [
-                        'css/style.css'
-                    ],
-                    test: 'css/style.css'
-                },
-                sections: {
-                    views: 'views/*.html',
-                    templates: 'templates/*.html'
-                },
-                data: {
-    			// Data to pass to templates
-                    version: "0.1.0",
-                    title: "test"
-                }
-            }
-        }
-    },
+      // htmlbuild: {
+      //     dist: {
+      //         src: 'index.html',
+      //         dest: 'samples/',
+      //         options: {
+      //             beautify: true,
+      //             prefix: '//some-cdn',
+      //             relative: true,
+      //             basePath: false,
+      //             scripts: {
+      //                 bundle: [
+      //                     '<%= fixturesPath %>/scripts/*.js',
+      //                     '!**/main.js',
+      //                 ],
+      //                 main: '<%= fixturesPath %>/scripts/main.js'
+      //             },
+      //             styles: {
+      //                 bundle: [
+      //                     '<%= fixturesPath %>/css/libs.css',
+      //                     '<%= fixturesPath %>/css/dev.css'
+      //                 ],
+      //                 test: '<%= fixturesPath %>/css/inline.css'
+      //             },
+      //             sections: {
+      //                 views: '<%= fixturesPath %>/views/**/*.html',
+      //                 templates: '<%= fixturesPath %>/templates/**/*.html',
+      //                 layout: {
+      //                     header: '<%= fixturesPath %>/layout/header.html',
+      //                     footer: '<%= fixturesPath %>/layout/footer.html'
+      //                 }
+      //             },
+      //             data: {
+      //                 // Data to pass to templates
+      //                 version: "0.1.0",
+      //                 title: "test",
+      //             },
+      //         }
+      //     }
+      // },
     watch: { // Compile everything into one task with Watch Plugin
       css: {
         files: '**/*.sass',
