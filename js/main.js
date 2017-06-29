6118f70a97bc30c454c0abd8ef89ef01eabed8a2
Dima-js <dimd98@ukr.net>
2017-06-29 19:00:41 +0300

    add many block

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-
+// $( document ).ready(function() {
+//
+//
+//        $('.wrap_menu ul li a ').hover(function(event){
+//            event.preventDefault();
+//            $(this).toggleClass('active');
+//            $('.item-main_menu').toggleClass('active');
+//
+//        });
+// });
 
